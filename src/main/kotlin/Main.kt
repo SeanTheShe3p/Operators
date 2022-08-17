c281ad5910e0fdf578e0ed372378c849b026876b
SeanTheShe3p <swiddowson@students.mchenry.edu>
2022-08-16 22:49:12 -0500

    added operators

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var ans1 = 0
+    var ans2 = 0
+    var ans3 = 0
+    var ans4 = 0
+    var a = 3
+    var b = 2
+    var c = 1
+    ans1 = a + b + c
+    println(ans1)
+    ans1++
+    ans2=ans1
+    println(ans2)
+    ans2--
+    println(ans2)
+    ans3=ans1/ans2
+    println(ans3)
+    ans1--
+    ans3=ans2%ans1
+    println(ans3)
+    ans4 = a - b - c
+    println(ans4)
 }
